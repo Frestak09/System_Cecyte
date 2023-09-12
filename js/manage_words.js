3317d5d8e955500cbec3395b8d61c6318233925e
Frestak09 <144755406+Frestak09@users.noreply.github.com>
2023-09-11 20:48:05 -0600

    Add files via upload

---
+++ js/manage_words.js
@@ -0,0 +1,67 @@
+/*
+
+  ____          _____               _ _           _       
+ |  _ \        |  __ \             (_) |         | |      
+ | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___ 
+ |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
+ | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
+ |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
+         __/ |                               __/ |        
+        |___/                               |___/         
+    
+____________________________________
+/ Si necesitas ayuda, contáctame en \
+\ https://parzibyte.me               /
+ ------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+Creado por Parzibyte (https://parzibyte.me). Este encabezado debe mantenerse intacto,
+excepto si este es un proyecto de un estudiante.
+*/
+new Vue({
+    el: "#app",
+    data: () => ({
+        words: [],
+        newWord: "",
+    }),
+    mounted() {
+        this.refreshWords();
+    },
+    methods: {
+        refreshWords() {
+            this.words = getWords();
+        },
+        saveWord() {
+            // Clean it again ._.
+            this.deleteWhiteSpaces();
+            const word = this.newWord.toUpperCase();
+            // Only save if it does not exist
+            if (this.words.indexOf(word) === -1) {
+                this.words.push(word);
+                saveWords(this.words);
+                this.newWord = "";
+            } else {
+                Swal.fire("Esta palabra ya existe");
+            }
+        },
+        async deleteWord(index) {
+            const result = await Swal.fire({
+                title: 'Borrar Palabra',
+                text: "Estas Seguro?",
+                icon: 'Preguntas',
+                showCancelButton: true,
+                cancelButtonText: 'No, Regresar Atras',
+                confirmButtonText: 'Si, Borrarlo '
+            });
+            if (!result.value) return;
+            this.words.splice(index, 1);
+            saveWords(this.words);
+        },
+        deleteWhiteSpaces() {
+            this.newWord = this.newWord.replace(/ /g, "")
+        }
+    }
+});
